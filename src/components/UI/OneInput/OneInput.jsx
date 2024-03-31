129bf5ff631c2f5149f49fc8a049b57dcf34923b
SecondNiceee <col1596321@gmail.com>
2024-03-31 21:06:41 +0300

    Updates

--- src/components/UI/OneInput/OneInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './OneInput.module.css'
-const OneInput = ( props ) => {
-    return (
-        <input style={props.style} {...props} type="text" className = {classes.OneInput}  />
-    );
-};
-
-export default OneInput;
