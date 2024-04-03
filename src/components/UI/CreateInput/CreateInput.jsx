3d0126a74bfcdc0618496f92f8ad361d541f276c
arsen00531 <arsendag505@gmail.com>
2024-04-03 05:25:22 +0300

    change structure to typescript

--- src/components/UI/CreateInput/CreateInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import cl from './CreateInput.module.css'
-const CreateInput = ({className , ...props}) => {
-    return (
-        <input className = {className ? [cl.CreateInput , className] : cl.CreateInput} type="text" {...props}  />
-    );
-};
-
-export default CreateInput;
