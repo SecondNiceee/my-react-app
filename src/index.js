b6bf949c095296eab24fcb14d33955bd97e9f3bb
SecondNiceee <col1596321@gmail.com>
2024-04-05 02:16:09 +0300

    dasda

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ const router = createBrowserRouter ([
 ])
 
 ReactDOM.render(
-        <App />,
+    //     <App />,
 
-    document.getElementById('root')
+    // document.getElementById('root')
 );
 
 
